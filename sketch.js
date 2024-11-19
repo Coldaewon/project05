0e78a1d6239c33fca7993ce77257a6173a30cb73
Coldaewon <deawon04072@gmail.com>
2024-11-19 21:40:14 +0900

    마우스 클릭했을때 폭죽이 터지게
    emitters로 여러 폭죽 배열을 만듬.

--- sketch.js
+++ sketch.js
@@ -1,69 +1,80 @@
-let emitter;
+let emitters = [];
 
 function setup() {
-  createCanvas(400, 400);
-  emitter = new emitter(width / 2, height);
-  
+  createCanvas(640, 360);
 }
 
 function draw() {
   background(0);
-emitter.run();
+  
+
+  for (let emitter of emitters) {
+    emitter.run();
+  }
 }
 
-class Emitter{
-  constructor(x,y){
-    this.origin = createVector(x,y);
+function mousePressed() {
+  
+  emitters.push(new Emitter(mouseX, mouseY));
+}
+
+class Emitter {
+  constructor(x, y) {
+    this.origin = createVector(x, y);
     this.particles = [];
   }
 
-  run(){
-    for(let i = this.particles.length - 1; i >= 0; i--){
-      this.particles[i].run();
-      if(this.particles[i].isDead()){
-        this.particles.splice(i,1);
+  run() {
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.run();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
       }
     }
 
-    if(random(1) < 0.05){
+
+    if (random(1) < 0.05) {
       this.addParticle();
     }
+  }
+
+  addParticle() {
+
+    for (let i = 0; i < 100; i++) {
+      this.particles.push(new Particle(this.origin.x, this.origin.y));
     }
- 
-  addParticle(){
-    for(let i = 0; i < 100; i++){
-        this.particles.push(new Particle(this.origin.x, this.origin.y));
-      }
   }
 }
 
-class Particle(
-  constructor(x, y){
-    this.pos = createVector(x, y);
-    this.vel = createVector(random(-1, 1), random(-3, 0));
-    this.acc = createVector(0, 0.1);
+class Particle {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-2, 2), random(-5, -1));
+    this.acceleration = createVector(0, 0.1);
     this.lifespan = 255;
     this.color = color(random(200, 255), random(100, 200), random(100, 255));
   }
 
-  run(){
+  run() {
     this.update();
     this.show();
   }
 
-  update(){
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.lifespan -= 2;
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 4;
   }
 
-  show(){
-    fill(red(this.color), green(this.color), blue(this.color), this.lifespan);
+  show() {
     noStroke();
-    circle(this.pos.x, this.pos.y, 8);
+    fill(red(this.color), green(this.color), blue(this.color), this.lifespan);
+    circle(this.position.x, this.position.y, 8);
   }
 
-  isDead(){
+  isDead() {
     return this.lifespan < 0;
   }
-)
+}
