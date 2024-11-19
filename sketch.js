7437dcbf7e8c90a9e869e650a50a927a42f94085
Coldaewon <deawon04072@gmail.com>
2024-11-19 21:22:42 +0900

    class Emitter 추가

--- sketch.js
+++ sketch.js
@@ -3,9 +3,30 @@ let emitter;
 function setup() {
   createCanvas(400, 400);
   emitter = new emitter(width / 2, height);
+  
 }
 
 function draw() {
-  background(220);
+  background(0);
+emitter.run();
 }
 
+class Emitter{
+  constructor(x,y){
+    this.origin = createVector(x,y);
+    this.particles = [];
+  }
+
+  run(){
+    for(let i = this.particles.length - 1; i >= 0; i--){
+      this.particles[i].run();
+      if(this.particles[i].isDead()){
+        this.particles.splice(i,1);
+      }
+    }
+    this.addParticle();
+  }
+  addParticle(){
+    this.particles.push(new this.particles(this.origin.x, this.origin.y));
+  }
+}
